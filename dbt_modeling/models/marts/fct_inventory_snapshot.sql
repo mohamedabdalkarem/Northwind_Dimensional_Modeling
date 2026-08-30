f8da933462d0785cdecba18779b01c4e4d033535
mohamedabdalkarem <mohamedabdalkareem976@gmail.com>
2026-08-30 22:39:32 +0300

    fct inventory

--- dbt_modeling/models/marts/fct_inventory_snapshot.sql
+++ dbt_modeling/models/marts/fct_inventory_snapshot.sql
@@ -1,7 +1,7 @@
 {{
     config(
         materialized='incremental',
-        unique_key='order_line_id',
+        unique_key='id',
         incremental_strategy='merge',
         cluster_by=['order_date_key', 'customer_key']
     )
@@ -9,7 +9,14 @@
 
 {% set lookback_days = var('fact_sales_lookback_days', 3) %}
 
-with inventory as(
+with inventory transactions as(
 select * from {{ ref('stg_northwind__inventory_transactions') }}
 ),
 
+products as (
+select * from {{ ref('stg_northwind__products') }}
+),
+
+inventory_snapshot as (
+    select
+    
